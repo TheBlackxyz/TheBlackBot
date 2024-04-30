2b7fa332978e67e20dc3e3d2f421815db398dcb1
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-04-30 21:42:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TheBlackxyz/TheBlackBot.git /TheBlackBot
+  git clone https://github.com/TheBlackxyz/TheBlackBot.git /RRbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheBlackBot
+  git clone $UPSTREAM_REPO /RRbot
 fi
-cd /TheBlackBot
+cd /RRbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
