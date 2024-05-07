40041c33b6521fa589283c7782101a59359cc512
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-05-07 22:41:25 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -54,10 +54,11 @@ async def remove_junkuser__db(bot, message):
     sts = await message.reply_text('IN PROGRESS.......')   
     start_time = time.time()
     total_users = await db.total_users_count()
+    done = 0
     blocked = 0
     deleted = 0
     failed = 0
-    done = 0
+    success = 0
     async for user in users:
         pti, sh = await clear_junk(int(user['id']), b_msg)
         if pti == False:
@@ -83,16 +84,17 @@ async def broadcast_group(bot, message):
     start_time = time.time()
     total_groups = await db.total_chat_count()
     done = 0
-    failed = ""
-    success = 0
+    failed = 0
+    Blocked = 0
     deleted = 0
+        
     async for group in groups:
         pti, sh, ex = await broadcast_messages_group(int(group['id']), b_msg)
         if pti == True:
-            if sh == "Succes":
+            if sh == "Blocked":
                 success += 1
         elif pti == False:
-            if sh == "deleted":
+            if sh == "Deleted":
                 deleted+=1 
                 failed += ex 
                 try:
@@ -121,12 +123,12 @@ async def junk_clear_group(bot, message):
     start_time = time.time()
     total_groups = await db.total_chat_count()
     done = 0
-    failed = ""
+    failed = 0
     deleted = 0
     async for group in groups:
         pti, sh, ex = await junk_group(int(group['id']), b_msg)        
         if pti == False:
-            if sh == "deleted":
+            if sh == "Deleted":
                 deleted+=1 
                 failed += ex 
                 try:
@@ -149,7 +151,7 @@ async def junk_clear_group(bot, message):
 async def broadcast_messages_group(chat_id, message):
     try:
         await message.copy(chat_id=chat_id)
-        return True, "Succes", 'mm'
+        return True, "Blocked", 'mm'
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await broadcast_messages_group(chat_id, message)
@@ -162,7 +164,7 @@ async def junk_group(chat_id, message):
     try:
         kk = await message.copy(chat_id=chat_id)
         await kk.delete(True)
-        return True, "Succes", 'mm'
+        return True, "Blocked", 'mm'
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await junk_group(chat_id, message)
@@ -176,7 +178,7 @@ async def clear_junk(user_id, message):
     try:
         key = await message.copy(chat_id=user_id)
         await key.delete(True)
-        return True, "Success"
+        return True, "Blocked"
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await clear_junk(user_id, message)
@@ -199,7 +201,7 @@ async def clear_junk(user_id, message):
 async def broadcast_messages(user_id, message):
     try:
         await message.copy(chat_id=user_id)
-        return True, "Success"
+        return True, "Blocked"
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await broadcast_messages(user_id, message)
