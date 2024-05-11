b8a5210b23d0568152f16a6b54ab576c8e428f05
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-05-11 18:45:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,6 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+GET_DATA = environ.get('GET_DATA', "True"), True)
 
 # Filters Configuration 
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
