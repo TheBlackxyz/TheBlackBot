d9ddbdb1f72756b40192a862cc6efa049da43897
TheBlackXYZ <155623566+TheBlackxyz@users.noreply.github.com>
2024-06-25 07:55:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-search-tamil-bot
 fi
-cd /Auto-search-tamil-bot
+cd /TheBlackBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
